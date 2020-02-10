580f7ab44d957169a26baaa0877dc4149b732a9a
Carlos Moraes <carloscpv@live.com>
2020-02-10 18:38:27 -0300

    fix: Remove nivel de acesso do token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -29,14 +29,14 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, deliveryman } = user;
+    const { id, name } = user;
     return res.json({
       user: {
         id,
         name,
         email,
       },
-      token: jwt.sign({ id, deliveryman }, authConfig.secret, {
+      token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
     });
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,7 +16,6 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
-    req.userDeliveryman = decoded.deliveryman;
 
     return next();
   } catch (err) {
